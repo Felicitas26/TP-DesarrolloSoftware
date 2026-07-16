f73b56a9e78a2eb0d9c5a4fe0f8102cefdbe82bf
LuliMovio <luisina.movio@gmail.com>
2026-07-16 15:16:33 -0300

    Update cliente.controller.js

--- src/controllers/cliente.controller.js
+++ src/controllers/cliente.controller.js
@@ -4,7 +4,7 @@ class ClienteController {
 
     async getAll(req, res) {
         try { 
-            const clientes = await clientesService.getAll();
+            const clientes = await clienteService.getAll();
             return res.status(200).json(clientes);
             }
         catch (error) {
@@ -31,11 +31,32 @@ class ClienteController {
                 });
         }
 
-    
-
     async create(req, res) {
         
-
+        const {
+            nombreCli, apellidoCli, telefonoCli, dniCli, emailCli, direccionCli, localidadCli } = req.body;
+        
+        const nuevoCliente = await clienteService.create(req.body);
+        
+        if ( !nombreCli || !apellidoCli || !telefonoCli || !dniCli || !emailCli || !direccionCli || !localidadCli) {
+            return res.status(400).json ({
+                error: "Todos los campos son obligatorios."
+            });
+            }
+       
+        if (!/^\d+$/.test(dniCli)) {
+           return res.status(400).json({
+                error: "El DNI solo puede contener números."
+            });
+        }
+       
+        if (!/^\d+$/.test(telefonoCli)) {
+           return res.status(400).json({
+               error: "El teléfono solo puede contener números."
+            });
+        }
+        
+        return res.status(200).json(nuevoCliente);
     }
 
     async update(req, res) {
