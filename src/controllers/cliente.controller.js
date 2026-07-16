352f0cb340bcad0b20893e161dc2280dbba7b87f
LuliMovio <luisina.movio@gmail.com>
2026-07-16 15:59:49 -0300

    Update cliente.controller.js

--- src/controllers/cliente.controller.js
+++ src/controllers/cliente.controller.js
@@ -35,35 +35,84 @@ class ClienteController {
         
         const {
             nombreCli, apellidoCli, telefonoCli, dniCli, emailCli, direccionCli, localidadCli } = req.body;
+
+        try {
         
-        const nuevoCliente = await clienteService.create(req.body);
-        
-        if ( !nombreCli || !apellidoCli || !telefonoCli || !dniCli || !emailCli || !direccionCli || !localidadCli) {
-            return res.status(400).json ({
-                error: "Todos los campos son obligatorios."
-            });
+            if ( !nombreCli || !apellidoCli || !telefonoCli || !dniCli || !emailCli || !direccionCli || !localidadCli) {
+                return res.status(400).json ({
+                    error: "Todos los campos son obligatorios."
+                });
+                }
+           
+            if (!/^\d+$/.test(dniCli)) {
+               return res.status(400).json({
+                    error: "El DNI solo puede contener números."
+                });
             }
-       
-        if (!/^\d+$/.test(dniCli)) {
-           return res.status(400).json({
-                error: "El DNI solo puede contener números."
-            });
-        }
-       
-        if (!/^\d+$/.test(telefonoCli)) {
-           return res.status(400).json({
-               error: "El teléfono solo puede contener números."
-            });
-        }
+           
+            if (!/^\d+$/.test(telefonoCli)) {
+               return res.status(400).json({
+                   error: "El teléfono solo puede contener números."
+                });
+            }
+            
+            const nuevoCliente = await clienteService.create(req.body);
+            return res.status(201).json(nuevoCliente);
+            }
         
-        return res.status(200).json(nuevoCliente);
+            catch (error) {
+                return res.status(500).json ({
+                error: error.message
+                });
+
     }
 
     async update(req, res) {
 
+        try {
+            const { id } = req.params;
+            const clienteUpdated = await clienteService.update(id, req.body);
+           
+            if (!clienteUpdated) {
+                return res.status(404).json({
+                   error: "No se ha encontrado al cliente."
+                });
+
+            }
+            return res.status(200).json({
+                mensaje: "Cliente actualizado correctamente.",
+                cliente: clienteUpdated
+        });
+        }
+            catch (error) {
+                return res.status(500).json ({
+                error: error.message
+                });
     }
 
     async delete(req, res) {
+        try {
+
+            const { id } = req.params;
+            const clienteDeleted = await clienteService.delete(id);
+    
+            if (!clienteDeleted) {
+                return res.status(404).json({
+                    error: "No se ha encontrado al cliente."
+                });
+        }
+
+        return res.status(200).json({
+            mensaje: "Cliente eliminado correctamente."
+        });
+
+    } catch (error) {
+
+        return res.status(500).json({
+            error: error.message
+        });
+
+    }
 
     }
 
