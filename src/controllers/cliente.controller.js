e61510cc453a8baa058bab1488a6c20d5f3d645c
LuliMovio <luisina.movio@gmail.com>
2026-07-16 14:49:17 -0300

    Update cliente.controller.js

--- src/controllers/cliente.controller.js
+++ src/controllers/cliente.controller.js
@@ -15,8 +15,23 @@ class ClienteController {
     }
 
     async getById(req, res) {
+       const { id }  = req.params;
+        try {
+            const cliente = await clienteService.getById(id);
+            if (!cliente) {
+                return res.status(404).json({ 
+                    error: `Cliente con ID ${id} no encontrado.`
+                       });
+                 }
+            return res.status(200).json(cliente);
+        }
+        catch (error) {
+            return res.status(500).json ({
+                error: error.message
+                });
+        }
 
-    }
+    
 
     async create(req, res) {
         
