f013a5f4eefe297685a49a3612a358c8fcbb0d6e
LuliMovio <luisina.movio@gmail.com>
2026-07-16 14:13:50 -0300

    Create cliente.controller.js

---
+++ src/controllers/cliente.controller.js
@@ -0,0 +1,27 @@
+import clienteService from "../services/cliente.service.js";
+
+class ClienteController {
+
+    async getAll(req, res) {
+
+    }
+
+    async getById(req, res) {
+
+    }
+
+    async create(req, res) {
+
+    }
+
+    async update(req, res) {
+
+    }
+
+    async delete(req, res) {
+
+    }
+
+}
+
+export default new ClienteController();
