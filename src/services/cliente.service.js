d2fab58aaa7407e0bbc7a5b63d8f4f595bfd3e21
LuliMovio <luisina.movio@gmail.com>
2026-07-21 08:57:55 -0300

    Update cliente.service.js

--- src/services/cliente.service.js
+++ src/services/cliente.service.js
@@ -1,1 +1,27 @@
+import db from "../../db.js";
 
+class ClienteService {
+
+    async getAll() {
+
+    }
+
+    async getById(id) {
+
+    }
+
+    async create(cliente) {
+
+    }
+
+    async update(id, cliente) {
+
+    }
+
+    async delete(id) {
+
+    }
+
+}
+
+export default new ClienteService();
