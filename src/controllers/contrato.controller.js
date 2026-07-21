0c447760d5dbd628d1735f0187feaf6ef5d0bd87
LuliMovio <luisina.movio@gmail.com>
2026-07-21 10:21:01 -0300

    Update contrato.controller.js

--- src/controllers/contrato.controller.js
+++ src/controllers/contrato.controller.js
@@ -1,1 +1,27 @@
+import contratoService from "../services/contrato.service.js";
 
+class ContratoController {
+
+    async getAll(req, res) {
+
+    }
+
+    async getById(req, res) {
+
+    }
+
+    async create(req, res) {
+
+    }
+
+    async update(req, res) {
+
+    }
+
+    async delete(req, res) {
+
+    }
+
+}
+
+export default new ContratoController();
