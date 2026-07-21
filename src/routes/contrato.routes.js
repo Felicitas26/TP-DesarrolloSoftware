d04fec1d55e643edb30385fd4f7301492bd6b040
LuliMovio <luisina.movio@gmail.com>
2026-07-21 10:11:57 -0300

    Update contrato.routes.js

--- src/routes/contrato.routes.js
+++ src/routes/contrato.routes.js
@@ -1,1 +1,12 @@
+import { Router } from "express";
+import contratoController from "../controllers/contrato.controller.js";
 
+const router = Router();
+
+router.get("/", contratoController.getAll);
+router.get("/:id", contratoController.getById);
+router.post("/", contratoController.create);
+router.put("/:id", contratoController.update);
+router.delete("/:id", contratoController.delete);
+
+export default router;
