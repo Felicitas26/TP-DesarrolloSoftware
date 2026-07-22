42c930afe45ad2b0741b5e6b5e3427d095fc475b
LuliMovio <luisina.movio@gmail.com>
2026-07-22 13:53:09 -0300

    Update detalleTarjeta.routes.js

--- src/routes/detalleTarjeta.routes.js
+++ src/routes/detalleTarjeta.routes.js
@@ -1,1 +1,12 @@
+import express from "express";
+import detalleTarjetaController from "../controllers/detalleTarjeta.controller.js";
 
+const router = express.Router();
+
+router.get("/", detalleTarjetaController.getAll);
+router.get("/:id", detalleTarjetaController.getById);
+router.post("/", detalleTarjetaController.create);
+router.put("/:id", detalleTarjetaController.update);
+router.delete("/:id", detalleTarjetaController.delete);
+
+export default router;
