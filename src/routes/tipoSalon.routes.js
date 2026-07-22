5e52a839e1e7438e435167ef51c3879560be105f
Felicitas26 <dipentimafelicitas@gmail.com>
2026-07-22 16:39:37 -0300

    Update tipoSalon.routes.js

--- src/routes/tipoSalon.routes.js
+++ src/routes/tipoSalon.routes.js
@@ -4,13 +4,9 @@ import tipoSalonController from "../controllers/tipoSalon.controller.js";
 const router = Router();
 
 router.get("/", tipoSalonController.getAll);
-
 router.get("/:id", tipoSalonController.getById);
-
 router.post("/", tipoSalonController.create);
-
 router.put("/:id", tipoSalonController.update);
-
 router.delete("/:id", tipoSalonController.delete);
 
 export default router;
