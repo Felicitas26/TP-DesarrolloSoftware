f231f46c4ae1f115a4057cd49867da5f01c499fa
Felicitas26 <dipentimafelicitas@gmail.com>
2026-07-22 16:36:24 -0300

    Create ubicacion.routes.js

---
+++ src/routes/ubicacion.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import ubicacionController from "../controllers/ubicacion.controller.js";
+
+const router = Router();
+
+router.get("/", ubicacionController.getAll);
+
+router.get("/:id", ubicacionController.getById);
+
+router.post("/", ubicacionController.create);
+
+router.put("/:id", ubicacionController.update);
+
+router.delete("/:id", ubicacionController.delete);
+
+export default router;
