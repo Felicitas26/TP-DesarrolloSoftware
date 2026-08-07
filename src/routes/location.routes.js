d4be35a26c7a8c940dc13eaa9128faf7d3ead4dd
NadiaBerrino <nanuzaghis20@gmail.com>
2026-08-07 10:21:37 -0300

    Update and rename ubicacion.routes.js to location.routes.js

---
+++ src/routes/location.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import locationController from "../controllers/location.controller.js";
+
+const router = Router();
+
+router.get("/", locationController.getAll);
+router.get("/:id", locationController.getById);
+router.post("/", locationController.create);
+router.put("/:id", locationController.update);
+router.delete("/:id", locationController.delete);
+
+export default router;
