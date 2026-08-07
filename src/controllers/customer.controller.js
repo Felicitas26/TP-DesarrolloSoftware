16c8bb105096f5a7614933a172aaf87268149ed9
Felicitas26 <dipentimafelicitas@gmail.com>
2026-08-07 09:46:49 -0300

    Fix import path for customer service
    traducciòn de las variables al inglès

--- src/controllers/customer.controller.js
+++ src/controllers/customer.controller.js
@@ -1,11 +1,11 @@
-import clienteService from "../services/cliente.service.js";
+import customerService from "../services/customer.service.js";
 
-class ClienteController {
+class CustomerController {
 
     async getAll(req, res) {
         try { 
-            const clientes = await clienteService.getAll();
-            return res.status(200).json(clientes);
+            const customer = await customerService.getAll();
+            return res.status(200).json(customers);
             }
         catch (error) {
             return res.status(500).json ({
@@ -17,13 +17,13 @@ class ClienteController {
     async getById(req, res) {
        const { id }  = req.params;
         try {
-            const cliente = await clienteService.getById(id);
-            if (!cliente) {
+            const customer = await customerService.getById(id);
+            if (!customer) {
                 return res.status(404).json({ 
                     error: `Cliente con ID ${id} no encontrado.`
                        });
                  }
-            return res.status(200).json(cliente);
+            return res.status(200).json(customer);
         }
         catch (error) {
             return res.status(500).json ({
@@ -34,30 +34,30 @@ class ClienteController {
     
     async create(req, res) {
         const {
-            nombreCli, apellidoCli, telefonoCli, dniCli, emailCli, direccionCli, localidadCli } = req.body;
+            nameCus, lastNameCus, phoneCus, dniCus, emailCus, adressCus, localityCus } = req.body;
 
         try {
         
-            if ( !nombreCli || !apellidoCli || !telefonoCli || !dniCli || !emailCli || !direccionCli || !localidadCli) {
+            if ( !nameCus || !lastNameCus || !phoneCus || !dniCus || !emailCus || !adressCus || !localityCus) {
                 return res.status(400).json ({
                     error: "Todos los campos son obligatorios."
                 });
                 }
            
-            if (!/^\d+$/.test(dniCli)) {
+            if (!/^\d+$/.test(dniCus)) {
                return res.status(400).json({
                     error: "El DNI solo puede contener números."
                 });
             }
            
-            if (!/^\d+$/.test(telefonoCli)) {
+            if (!/^\d+$/.test(phoneCus)) {
                return res.status(400).json({
                    error: "El teléfono solo puede contener números."
                 });
             }
             
-            const nuevoCliente = await clienteService.create(req.body);
-            return res.status(201).json(nuevoCliente);
+            const newCustomer = await customerService.create(req.body);
+            return res.status(201).json(newCustomer);
             }
         
             catch (error) {
@@ -71,16 +71,16 @@ class ClienteController {
 
         try {
             const { id } = req.params;
-            const clienteUpdated = await clienteService.update(id, req.body);
+            const customerUpdated = await customerService.update(id, req.body);
            
-            if (!clienteUpdated) {
+            if (!customerUpdated) {
                 return res.status(404).json({
                    error: "No se ha encontrado al cliente."
                 });
             }
             return res.status(200).json({
                 mensaje: "Cliente actualizado correctamente.",
-                cliente: clienteUpdated
+                customer: customerUpdated
         });
         }
             catch (error) {
@@ -93,9 +93,9 @@ class ClienteController {
         try {
 
             const { id } = req.params;
-            const clienteDeleted = await clienteService.delete(id);
+            const customerDeleted = await customerService.delete(id);
     
-            if (!clienteDeleted) {
+            if (!customerDeleted) {
                 return res.status(404).json({
                     error: "No se ha encontrado al cliente."
                 });
@@ -117,4 +117,4 @@ class ClienteController {
 
 }
 
-export default new ClienteController();
+export default new CustomerController();
