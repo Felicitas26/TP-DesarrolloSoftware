a65e229d6efa1a3c8d059ffa3d18522ac17e44a5
Luli <luisina.movio@gmail.com>
2026-08-11 20:52:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import db from './db.js';
 import express from 'express';
+
 import loungeTypeRoutes from './src/routes/loungeType.routes.js';
 import clientRoutes from './src/routes/client.routes.js';
 import extraServiceRoutes from "./src/routes/extraService.routes.js";
@@ -9,19 +9,6 @@ import cardDetailRoutes from "./src/routes/cardDetail.routes.js";
 const app = express();
 
 app.use(express.json());
-app.get('/client', async (req, res) => {
-  try {
-    const [filas] = await db.query('SELECT * FROM client');
-    
-    res.json({
-      mensaje: "¡Clientes cargados con éxito!",
-      clientes: filas
-    });
-  } catch (error) {
-    console.error("Error al consultar los clientes:", error);
-    res.status(500).json({ error: "Hubo un error al obtener los clientes" });
-  }
-});
 
 app.use('/api/loungeType', loungeTypeRoutes);
 app.use('/api/client', clientRoutes);
@@ -29,7 +16,6 @@ app.use("/extraService", extraServiceRoutes);
 app.use("/contract", contractRoutes);
 app.use("/cardDetail", cardDetailRoutes);
 
-
 app.listen(3000, () => {
-  console.log('¡Servidor del TP encendido y escuchando en el puerto 3000!');
-}); 
+    console.log('¡Servidor del TP encendido y escuchando en el puerto 3000!');
+});
