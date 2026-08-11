4489b68731ae72249d45bb6ae2351a259f87b847
Luli <luisina.movio@gmail.com>
2026-08-11 20:22:03 -0300

    Update reservation.service.js

--- src/services/reservation.service.js
+++ src/services/reservation.service.js
@@ -1,1 +1,92 @@
+```js
+import db from "../../db.js";
 
+class ReservationService {
+
+    async getAll() {
+        const [rows] = await db.execute(
+            "SELECT * FROM Reservation"
+        );
+
+        return rows;
+    }
+
+    async getById(id) {
+        const [rows] = await db.execute(
+            "SELECT * FROM Reservation WHERE idReservation = ?",
+            [id]
+        );
+
+        return rows[0];
+    }
+
+    async create(reservation) {
+        const {
+            dateReservation,
+            status,
+            guestQuantity
+        } = reservation;
+
+        const [result] = await db.execute(
+            `INSERT INTO Reservation
+            (dateReservation, status, guestQuantity)
+            VALUES (?, ?, ?)`,
+            [
+                dateReservation,
+                status,
+                guestQuantity
+            ]
+        );
+
+        return {
+            idReservation: result.insertId,
+            dateReservation,
+            status,
+            guestQuantity
+        };
+    }
+
+    async update(id, reservation) {
+        const {
+            dateReservation,
+            status,
+            guestQuantity
+        } = reservation;
+
+        const [result] = await db.execute(
+            `UPDATE Reservation
+            SET dateReservation = ?,
+                status = ?,
+                guestQuantity = ?
+            WHERE idReservation = ?`,
+            [
+                dateReservation,
+                status,
+                guestQuantity,
+                id
+            ]
+        );
+
+        if (result.affectedRows === 0) {
+            return null;
+        }
+
+        return this.getById(id);
+    }
+
+    async delete(id) {
+        const [result] = await db.execute(
+            "DELETE FROM Reservation WHERE idReservation = ?",
+            [id]
+        );
+
+        if (result.affectedRows === 0) {
+            return null;
+        }
+
+        return true;
+    }
+}
+
+export default new ReservationService();
+```
