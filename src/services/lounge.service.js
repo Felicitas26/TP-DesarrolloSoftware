9c0a9d293313078f81a4f196625bde0b80d2744d
Luli <luisina.movio@gmail.com>
2026-08-11 20:14:49 -0300

    Update lounge.service.js

--- src/services/lounge.service.js
+++ src/services/lounge.service.js
@@ -1,22 +1,92 @@
+```js
 import db from "../../db.js";
 
 class LoungeService {
 
-  async create(data) {
+    async getAll() {
+        const [rows] = await db.execute(
+            "SELECT * FROM Lounge"
+        );
 
-    const { typeLoungeId } = data;
-
-    const type = await db.LoungeType.findByPk(typeLoungeId);
-
-    if (!type) {
-      throw new Error("El tipo de salón indicado no existe.");
+        return rows;
     }
 
-    const lounge = await db.Lounge.create(data);
+    async getById(id) {
+        const [rows] = await db.execute(
+            "SELECT * FROM Lounge WHERE idLounge = ?",
+            [id]
+        );
 
-    return lounge;
-  }
+        return rows[0];
+    }
 
+    async create(lounge) {
+        const {
+            name,
+            loungeAddress,
+            idLocation
+        } = lounge;
+
+        const [result] = await db.execute(
+            `INSERT INTO Lounge
+            (name, loungeAddress, idLocation)
+            VALUES (?, ?, ?)`,
+            [
+                name,
+                loungeAddress,
+                idLocation
+            ]
+        );
+
+        return {
+            idLounge: result.insertId,
+            name,
+            loungeAddress,
+            idLocation
+        };
+    }
+
+    async update(id, lounge) {
+        const {
+            name,
+            loungeAddress,
+            idLocation
+        } = lounge;
+
+        const [result] = await db.execute(
+            `UPDATE Lounge
+            SET name = ?,
+                loungeAddress = ?,
+                idLocation = ?
+            WHERE idLounge = ?`,
+            [
+                name,
+                loungeAddress,
+                idLocation,
+                id
+            ]
+        );
+
+        if (result.affectedRows === 0) {
+            return null;
+        }
+
+        return this.getById(id);
+    }
+
+    async delete(id) {
+        const [result] = await db.execute(
+            "DELETE FROM Lounge WHERE idLounge = ?",
+            [id]
+        );
+
+        if (result.affectedRows === 0) {
+            return null;
+        }
+
+        return true;
+    }
 }
 
 export default new LoungeService();
+```
