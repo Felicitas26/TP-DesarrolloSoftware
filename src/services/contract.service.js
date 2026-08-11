a15d8509f4df252044f587b8c23a88d8ec87ce68
Luli <luisina.movio@gmail.com>
2026-08-11 20:45:41 -0300

    Update contract.service.js

--- src/services/contract.service.js
+++ src/services/contract.service.js
@@ -1,97 +1,26 @@
-import db from "../../db.js";
+import contractModel from "../models/contract.model.js";
 
 class ContractService {
 
     async getAll() {
-        const [rows] = await db.execute(
-            "SELECT * FROM Contract"
-        );
-
-        return rows;
+        return await contractModel.getAll();
     }
 
     async getById(id) {
-        const [rows] = await db.execute(
-            "SELECT * FROM Contract WHERE idContract = ?",
-            [id]
-        );
-
-        return rows[0];
+        return await contractModel.getById(id);
     }
 
     async create(contract) {
-        const {
-            eventStartTime,
-            eventEndTime,
-            dateContract,
-            finalValue
-        } = contract;
-
-        const [result] = await db.execute(
-            `INSERT INTO Contract
-            (eventStartTime, eventEndTime, dateContract, finalValue)
-            VALUES (?, ?, ?, ?)`,
-            [
-                eventStartTime,
-                eventEndTime,
-                dateContract,
-                finalValue
-            ]
-        );
-
-        return {
-            idContract: result.insertId,
-            eventStartTime,
-            eventEndTime,
-            dateContract,
-            finalValue
-        };
+        return await contractModel.create(contract);
     }
 
     async update(id, contract) {
-        const {
-            eventStartTime,
-            eventEndTime,
-            dateContract,
-            finalValue
-        } = contract;
-
-        const [result] = await db.execute(
-            `UPDATE Contract
-            SET eventStartTime = ?,
-                eventEndTime = ?,
-                dateContract = ?,
-                finalValue = ?
-            WHERE idContract = ?`,
-            [
-                eventStartTime,
-                eventEndTime,
-                dateContract,
-                finalValue,
-                id
-            ]
-        );
-
-        if (result.affectedRows === 0) {
-            return null;
-        }
-
-        return this.getById(id);
+        return await contractModel.update(id, contract);
     }
 
     async delete(id) {
-        const [result] = await db.execute(
-            "DELETE FROM Contract WHERE idContract = ?",
-            [id]
-        );
-
-        if (result.affectedRows === 0) {
-            return null;
-        }
-
-        return true;
+        return await contractModel.delete(id);
     }
 }
 
 export default new ContractService();
-
