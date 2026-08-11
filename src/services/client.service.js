cedf0fc92d9382fb10e07e7a0fbf3cf046b73334
Luli <luisina.movio@gmail.com>
2026-08-11 20:40:58 -0300

    Update client.service.js

--- src/services/client.service.js
+++ src/services/client.service.js
@@ -1,114 +1,27 @@
-import db from "../../db.js";
+import clientModel from "../models/client.model.js";
 
 class ClientService {
 
     async getAll() {
-        const [rows] = await db.execute(
-            "SELECT * FROM Client"
-        );
-
-        return rows;
+        return await clientModel.getAll();
     }
 
     async getById(id) {
-        const [rows] = await db.execute(
-            "SELECT * FROM Client WHERE idCli = ?",
-            [id]
-        );
-
-        return rows[0];
+        return await clientModel.getById(id);
     }
 
     async create(client) {
-        const {
-            nameCli,
-            lastNameCli,
-            phoneCli,
-            dniCli,
-            emailCli,
-            addressCli,
-            localityCli
-        } = client;
-
-        const [result] = await db.execute(
-            `INSERT INTO Client
-            (nameCli, lastNameCli, phoneCli, dniCli, emailCli, addressCli, localityCli)
-            VALUES (?, ?, ?, ?, ?, ?, ?)`,
-            [
-                nameCli,
-                lastNameCli,
-                phoneCli,
-                dniCli,
-                emailCli,
-                addressCli,
-                localityCli
-            ]
-        );
-
-        return {
-            idCli: result.insertId,
-            nameCli,
-            lastNameCli,
-            phoneCli,
-            dniCli,
-            emailCli,
-            addressCli,
-            localityCli
-        };
+        return await clientModel.create(client);
     }
 
     async update(id, client) {
-        const {
-            nameCli,
-            lastNameCli,
-            phoneCli,
-            dniCli,
-            emailCli,
-            addressCli,
-            localityCli
-        } = client;
-
-        const [result] = await db.execute(
-            `UPDATE Client
-            SET nameCli = ?,
-                lastNameCli = ?,
-                phoneCli = ?,
-                dniCli = ?,
-                emailCli = ?,
-                addressCli = ?,
-                localityCli = ?
-            WHERE idCli = ?`,
-            [
-                nameCli,
-                lastNameCli,
-                phoneCli,
-                dniCli,
-                emailCli,
-                addressCli,
-                localityCli,
-                id
-            ]
-        );
-
-        if (result.affectedRows === 0) {
-            return null;
-        }
-
-        return this.getById(id);
+        return await clientModel.update(id, client);
     }
 
     async delete(id) {
-        const [result] = await db.execute(
-            "DELETE FROM Client WHERE idCli = ?",
-            [id]
-        );
-
-        if (result.affectedRows === 0) {
-            return null;
-        }
-
-        return true;
+        return await clientModel.delete(id);
     }
 }
 
 export default new ClientService();
+
