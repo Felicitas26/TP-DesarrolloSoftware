955f6eb98720212b2cfef99e479772ee83507765
Luli <luisina.movio@gmail.com>
2026-08-11 21:37:39 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-import express from 'express';
-
-import clientRoutes from './src/routes/client.routes.js';
+import express from "express";
+import cors from "cors";
+import clientRoutes from "./src/routes/client.routes.js";
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
-app.use('/api/client', clientRoutes);
+app.use("/api/client", clientRoutes);
 
 app.listen(3000, () => {
-    console.log('¡Servidor del TP encendido y escuchando en el puerto 3000!');
+    console.log("Server running on port 3000");
 });
