171ac12669feb5892bbb79ae303867efcfef0683
Luli <luisina.movio@gmail.com>
2026-08-11 21:00:01 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import ClientList from "./pages/clientes/clientList";
 import NewClient from "./pages/clientes/newClient";
+import EditClient from "./pages/clientes/editClient";
+
 import "./App.css";
 
 function App() {
-  return (
-    <NewClient />
-  );
+
+    return (
+        <BrowserRouter>
+
+            <Routes>
+
+                <Route path="/client" element={<ClientList />} />
+
+                <Route path="/client/new" element={<NewClient />} />
+
+                <Route path="/client/edit/:id" element={<EditClient />} />
+
+            </Routes>
+
+        </BrowserRouter>
+    );
 }
 
 export default App;
