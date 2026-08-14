6ac6bc4b0f7f7d24888ecdde4ea6ea68a1b7331d
LuliMovio <luisina.movio@gmail.com>
2026-08-14 17:58:32 -0300

    .

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import clientRoutes from "../src/routes/client.routes.js";
+import clientRoutes from "./src/routes/client.routes.js";
 
 const app = express();
 
