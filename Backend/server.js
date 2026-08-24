ba61bfe406278a0194f4636d53c7bd6800d180c7
NadiaBerrino <nanuzaghis20@gmail.com>
2026-08-24 16:50:50 -0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import clientRoutes from "./src/routes/client.routes.js";
+import loungeRoutes from "./src/routes/lounge.routes.js";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/client", clientRoutes);
+app.use("/api/lounge", loungeRoutes);
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
